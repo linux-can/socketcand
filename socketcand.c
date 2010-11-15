106dde63cf1340469f607708fc000c7a5d982d18
Jan-Niklas Meier <dschanoeh@googlemail.com>
2010-11-15 10:19:52 +0100

    changed transmission of error frames

--- socketcand.c
+++ socketcand.c
@@ -64,6 +64,7 @@
 
 #include <linux/can.h>
 #include <linux/can/bcm.h>
+#include <linux/can/error.h>
 #include <linux/can/netlink.h>
 
 #include <libsocketcan.h>
@@ -262,17 +263,35 @@ int main(int argc, char **argv)
             ifr.ifr_ifindex = caddr.can_ifindex;
             ioctl(sc, SIOCGIFNAME, &ifr);
 
-            sprintf(rxmsg, "< %s f %03X %d ", ifr.ifr_name,
-                msg.msg_head.can_id, msg.frame.can_dlc);
+            /* Check if this is an error frame */
+            if(msg.msg_head.can_id & 0x20000000) {
+                if(msg.frame.can_dlc != CAN_ERR_DLC) {
+                    printf("Error frame has a wrong DLC!\n");
+                } else {
+                    sprintf(rxmsg, "< %s e %03X ", ifr.ifr_name,
+                        msg.msg_head.can_id);
 
-            for ( i = 0; i < msg.frame.can_dlc; i++)
-                sprintf(rxmsg + strlen(rxmsg), "%02X ",
-                    msg.frame.data[i]);
+                    for ( i = 0; i < msg.frame.can_dlc; i++)
+                        sprintf(rxmsg + strlen(rxmsg), "%02X ",
+                            msg.frame.data[i]);
 
-            /* delimiter '\0' for Adobe(TM) Flash(TM) XML sockets */
-            strcat(rxmsg, ">\0");
+                    /* delimiter '\0' for Adobe(TM) Flash(TM) XML sockets */
+                    strcat(rxmsg, ">\0");
+                    send(sa, rxmsg, strlen(rxmsg) + 1, 0);
+                }
+            } else {
+                sprintf(rxmsg, "< %s f %03X %d ", ifr.ifr_name,
+                    msg.msg_head.can_id, msg.frame.can_dlc);
 
-            send(sa, rxmsg, strlen(rxmsg) + 1, 0);
+                for ( i = 0; i < msg.frame.can_dlc; i++)
+                    sprintf(rxmsg + strlen(rxmsg), "%02X ",
+                        msg.frame.data[i]);
+
+                /* delimiter '\0' for Adobe(TM) Flash(TM) XML sockets */
+                strcat(rxmsg, ">\0");
+
+                send(sa, rxmsg, strlen(rxmsg) + 1, 0);
+            }
         }
 
 
