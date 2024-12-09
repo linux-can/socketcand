c42f31d2d5423d6964cceb0a58b03a15c2501151
Marc Kleine-Budde <mkl@pengutronix.de>
2024-12-09 11:35:17 +0100

    canctl: fix coding style

--- src/canctl.h
+++ src/canctl.h
@@ -1,9 +1,8 @@
 /**
  * canctrl.h
- * 
+ *
  * Functions to configure the CAN interfaces (bitrates, control modes, etc...)
  */
-
 
 #ifndef __CANCTRL_H__
 #define __CANCTRL_H__
@@ -14,20 +13,20 @@
 
 /**
  * canctl_set_bittiming - Set the interface bittiming.
- * 
+ *
  * @param bus_name name of the can interface to configure.
  * @param buff_bittiming_conf buffer which contains the remaining config of the interface.
  * @param buff_length config buffer length.
- * 
+ *
  * returns 0 - ok / -1 error
  */
 int canctl_set_bittiming(const char *bus_name, const char *buff_bittiming_conf, int buff_length);
 
 /**
  * canctl_start_iface - Start the can interface.
- * 
+ *
  * @param bus_name name of the can interface to start.
- * 
+ *
  * returns 0 - ok / -1 error
  */
 static inline int canctl_start_iface(const char *bus_name)
@@ -37,11 +36,11 @@ static inline int canctl_start_iface(const char *bus_name)
 
 /**
  * canctl_set_control_modes - set up control modes for given interface.
- * 
+ *
  * @param bus_name name of the can interface to configure.
  * @param buff_control_modes_conf buffer which contains the control modes configuration.
  * @param buff_length config buffer length.
- * 
+ *
  * returns 0 - ok / -1 error
  */
 int canctl_set_control_modes(const char *bus_name, const char *buff_control_modes_conf, int buff_length);
@@ -62,7 +61,7 @@ static inline int canctl_start_iface(const char *bus_name)
 
 static inline int canctl_set_control_modes(const char *bus_name, const char *buff_control_modes_conf, int buff_length)
 {
-    return -EINVAL;
+	return -EINVAL;
 }
 
 #endif
